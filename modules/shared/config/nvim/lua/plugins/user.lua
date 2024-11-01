91a686859280cd387c3d707defdcea9ed1ad28de
Sam Tay <samctay@pm.me>
2024-11-01 11:01:34 -0400

    Fix static-ls formatting
    Static-ls temporarily dropped support for `textDocument/formatting`.
    See https://github.com/josephsumabat/static-ls/pull/122

--- modules/shared/config/nvim/lua/plugins/user.lua
+++ modules/shared/config/nvim/lua/plugins/user.lua
@@ -172,4 +172,24 @@ return {
   -- },
   { "luc-tielen/telescope_hoogle" },
   { "alx741/yesod.vim" },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      notify_on_error = true,
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
+        return { timeout_ms = 1000, lsp_format = "fallback" }
+      end,
+      formatters_by_ft = {
+        haskell = { "mwb_format", "fourmolu", stop_after_first = true },
+      },
+      formatters = {
+        mwb_format = {
+          command = "run-format",
+          args = { "--stdio" },
+        },
+      },
+    },
+  },
 }
