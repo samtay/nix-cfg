b84d698617f8f7ec734e82679c0803185df03298
Sam Tay <samctay@pm.me>
2024-11-23 22:42:06 -0500

    Add snippets for running tests with ghciwatch

--- modules/shared/config/nvim/lua/plugins/user.lua
+++ modules/shared/config/nvim/lua/plugins/user.lua
@@ -65,7 +65,12 @@ return {
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      -- include the default astronvim config that calls the setup call
+      require "astronvim.plugins.configs.luasnip"(plugin, opts)
+      -- load snippets paths
+      require("luasnip.loaders.from_vscode").lazy_load {
+        paths = { vim.fn.stdpath "config" .. "/snippets" },
+      }
       -- add more custom luasnip configuration such as filetype extend or custom snippets
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
