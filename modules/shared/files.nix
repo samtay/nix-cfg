9d3661655db12500655287e70acbfd3d3cdefbba
Sam Tay <samctay@pm.me>
2025-04-11 12:01:48 -0400

    Add zed config

--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -3,6 +3,9 @@
 {
   ".zshrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nix-cfg/modules/shared/config/zsh/zshrc";
   ".zimrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nix-cfg/modules/shared/config/zsh/zimrc";
+  ".config/zed/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nix-cfg/modules/shared/config/zed/settings.json";
+  ".config/zed/keymap.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nix-cfg/modules/shared/config/zed/keymap.json";
+  ".config/zed/tasks.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nix-cfg/modules/shared/config/zed/tasks.json";
   ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nix-cfg/modules/shared/config/nvim";
   ".psqlrc".text = ''
     \x auto
